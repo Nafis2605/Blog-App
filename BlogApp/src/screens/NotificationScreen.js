c72b5a03d2ee44326b345a29fcdad3419daaffb9
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-12-16 19:57:43 +0600

    Notification Screen is Added

--- BlogApp/src/screens/NotificationScreen.js
+++ BlogApp/src/screens/NotificationScreen.js
@@ -1,11 +1,39 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-
-const NotificationScreen = () => {
+import React, { useState } from "react";
+import { View, StyleSheet, AsyncStorage } from "react-native";
+import { Text, Card, Button, Avatar, Header } from "react-native-elements";
+import { AuthContext } from "../providers/AuthProvider";
+import HeaderHome from "../components/HeaderHome";
+const NotificationScreen = (props) => {
     return (
-        <View>
-            <Text style={styles.textStyle}>Notification Screen</Text>
-        </View>
+        <AuthContext.Consumer>
+            {(auth) => (
+                <View style={styles.viewStyle}>
+                    <HeaderHome
+                        DrawerFunction={() => {
+                            props.navigation.toggleDrawer();
+                        }}
+                    />
+
+                    <Card>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <Avatar
+                                containerStyle={{ backgroundColor: "cyan" }}
+                                rounded
+                                icon={{
+                                    name: "thumbs-o-up",
+                                    type: "font-awesome",
+                                    color: "black",
+                                }}
+                                activeOpacity={1}
+                            />
+                            <Text style={{ paddingHorizontal: 10 }}>
+                                Faiyaz Arsad Navid Liked Your Post.
+              </Text>
+                        </View>
+                    </Card>
+                </View>
+            )}
+        </AuthContext.Consumer>
     );
 };
 
